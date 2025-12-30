570c156071b82fb662dc6401bf05828386875f81
student <student@DESKTOP-6IRC4SO>
2025-12-30 19:32:51 +0700

    -

--- RTS/RTS.cpp
+++ RTS/RTS.cpp
@@ -89,7 +89,7 @@ void ShowBitmap(HDC hDC,int x, int y,int x1,int y1,HBITMAP hBitmapBall, bool alp
 void InitWindows()
 {
     SetProcessDPIAware();
-    window.hWnd = CreateWindow("edit", 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0);
+    //window.hWnd = CreateWindow("edit", 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0);
 
     RECT r;
     GetClientRect(window.hWnd, &r);
@@ -251,3 +251,154 @@ int APIENTRY wWinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPWSTR lpCmdLi
 //    }
 //    return (INT_PTR)FALSE;
 //}
+
+/*void ShowRacketAndBall()
+{
+    ShowBitmap(window.context, 0, 0, window.width, window.height, hBack);//задний фон
+    ShowBitmap(window.context, racket.x - racket.width / 2., racket.y, racket.width, racket.height, racket.hBitmap);// ракетка игрока
+
+    if (ball.dy < 0 && (enemy.x - racket.width / 4 > ball.x || ball.x > enemy.x + racket.width / 4))
+    {
+        //имитируем разумность оппонента. на самом деле, компьютер никогда не проигрывает, и мы не считаем попадает ли его ракетка по шарику
+        //вместо этого, мы всегда делаем отскок от потолка, а раектку противника двигаем - подставляем под шарик
+        //движение будет только если шарик летит вверх, и только если шарик по оси X выходит за пределы половины длины ракетки
+        //в этом случае, мы смешиваем координаты ракетки и шарика в пропорции 9 к 1
+        enemy.x = ball.x * .1 + enemy.x * .9;
+    }
+
+    ShowBitmap(window.context, enemy.x - racket.width / 2, 0, racket.width, racket.height, enemy.hBitmap);//ракетка оппонента
+    ShowBitmap(window.context, ball.x - ball.rad, ball.y - ball.rad, 2 * ball.rad, 2 * ball.rad, ball.hBitmap, true);// шарик
+}
+
+void LimitRacket()
+{
+    racket.x = max(racket.x, racket.width / 2.);//если коодината левого угла ракетки меньше нуля, присвоим ей ноль
+    racket.x = min(racket.x, window.width - racket.width / 2.);//аналогично для правого угла
+}
+
+void CheckWalls()
+{
+    if (ball.x < ball.rad || ball.x > window.width - ball.rad)
+    {
+        ball.dx *= -1;
+        ProcessSound("bounce.wav");
+    }
+}
+
+void CheckRoof()
+{
+    if (ball.y < ball.rad + racket.height)
+    {
+        ball.dy *= -1;
+        ProcessSound("bounce.wav");
+    }
+}
+
+bool tail = false;
+
+void CheckFloor()
+{
+    if (ball.y > window.height - ball.rad - racket.height)//шарик пересек линию отскока - горизонталь ракетки
+    {
+        if (!tail && ball.x >= racket.x - racket.width / 2. - ball.rad && ball.x <= racket.x + racket.width / 2. + ball.rad)//шарик отбит, и мы не в режиме обработки хвоста
+        {
+            game.score++;//за каждое отбитие даем одно очко
+            ball.speed += 5. / game.score;//но увеличиваем сложность - прибавляем скорости шарику
+            ball.dy *= -1;//отскок
+            racket.width -= 10. / game.score;//дополнительно уменьшаем ширину ракетки - для сложности
+            ProcessSound("bounce.wav");//играем звук отскока
+        }
+        else
+        {//шарик не отбит
+
+            tail = true;//дадим шарику упасть ниже ракетки
+
+            if (ball.y - ball.rad > window.height)//если шарик ушел за пределы окна
+            {
+                game.balls--;//уменьшаем количество "жизней"
+
+                ProcessSound("fail.wav");//играем звук
+
+                if (game.balls < 0) { //проверка условия окончания "жизней"
+
+                    MessageBoxA(window.hWnd, "game over", "", MB_OK);//выводим сообщение о проигрыше
+                    InitGame();//переинициализируем игру
+                }
+
+                ball.dy = (rand() % 65 + 35) / 100.;//задаем новый случайный вектор для шарика
+                ball.dx = -(1 - ball.dy);
+                ball.x = racket.x;//инициализируем координаты шарика - ставим его на ракетку
+                ball.y = racket.y - ball.rad;
+                game.action = false;//приостанавливаем игру, пока игрок не нажмет пробел
+                tail = false;
+            }
+        }
+    }
+}
+
+void ProcessRoom()
+{
+    //обрабатываем стены, потолок и пол. принцип - угол падения равен углу отражения, а значит, для отскока мы можем просто инвертировать часть вектора движения шарика
+    CheckWalls();
+    CheckRoof();
+    CheckFloor();
+}
+
+void ProcessBall()
+{
+    if (game.action)
+    {
+        //если игра в активном режиме - перемещаем шарик
+        ball.x += ball.dx * ball.speed;
+        ball.y += ball.dy * ball.speed;
+    }
+    else
+    {
+        //иначе - шарик "приклеен" к ракетке
+        ball.x = racket.x;
+    }
+}
+
+void InitWindow()
+{
+    SetProcessDPIAware();
+    window.hWnd = CreateWindow("edit", 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0);
+
+    RECT r;
+    GetClientRect(window.hWnd, &r);
+    window.device_context = GetDC(window.hWnd);//из хэндла окна достаем хэндл контекста устройства для рисования
+    window.width = r.right - r.left;//определяем размеры и сохраняем
+    window.height = r.bottom - r.top;
+    window.context = CreateCompatibleDC(window.device_context);//второй буфер
+    SelectObject(window.context, CreateCompatibleBitmap(window.device_context, window.width, window.height));//привязываем окно к контексту
+    GetClientRect(window.hWnd, &r);
+
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    
+    InitWindow();//здесь инициализируем все что нужно для рисования в окне
+    InitGame();//здесь инициализируем переменные игры
+
+    mciSendString(TEXT("play ..\\Debug\\music.mp3 repeat"), NULL, 0, NULL);
+    ShowCursor(NULL);
+    
+    while (!GetAsyncKeyState(VK_ESCAPE))
+    {
+        ShowRacketAndBall();//рисуем фон, ракетку и шарик
+        ShowScore();//рисуем очик и жизни
+        BitBlt(window.device_context, 0, 0, window.width, window.height, window.context, 0, 0, SRCCOPY);//копируем буфер в окно
+        Sleep(16);//ждем 16 милисекунд (1/количество кадров в секунду)
+
+        ProcessInput();//опрос клавиатуры
+        LimitRacket();//проверяем, чтобы ракетка не убежала за экран
+        ProcessBall();//перемещаем шарик
+        ProcessRoom();//обрабатываем отскоки от стен и каретки, попадание шарика в картетку
+    }
+
+}
+*/
